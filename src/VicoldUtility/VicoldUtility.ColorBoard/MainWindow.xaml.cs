a42e960633c8c1c8e5411e60e5ba9a82e8627b17
Dustray <yinxiyue1995@outlook.com>
2020-08-19 23:17:14 +0800

    ds

--- src/VicoldUtility/VicoldUtility.ColorBoard/MainWindow.xaml.cs
+++ src/VicoldUtility/VicoldUtility.ColorBoard/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VicoldUtility.ColorBoard.Views;
 
 namespace VicoldUtility.ColorBoard
 {
@@ -23,6 +24,11 @@ namespace VicoldUtility.ColorBoard
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void colorModemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new ColorModemWindow().Show();
         }
     }
 }
