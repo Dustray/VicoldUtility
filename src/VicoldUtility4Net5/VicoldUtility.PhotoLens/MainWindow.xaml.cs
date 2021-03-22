79f98e634b4db8c00b94ea6229b9d06100ece828
Dustray <yinxiyue1995@outlook.com>
2021-03-22 23:20:48 +0800

    photolens

---
+++ src/VicoldUtility4Net5/VicoldUtility.PhotoLens/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VicoldUtility.PhotoLens
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Handled = false;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            //仅支持文件的拖放
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+            //获取拖拽的文件
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var file = files.First();
+
+
+
+        }
+    }
+}
