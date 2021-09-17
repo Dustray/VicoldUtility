352468b87f885d1a6ba829791d45587dfe9bafcf
Dustray <yinxiyue1995@outlook.com>
2021-09-17 18:04:07 +0800

    gis 4 mchsq

--- src/VicoldUtility4Net5/VicoldGis/Algorithms/ContourVicoldAlgo.cs
+++ src/VicoldUtility4Net5/VicoldGis/Algorithms/ContourVicoldAlgo.cs
@@ -6,47 +6,47 @@ using VicoldGis.Algorithms.Entities;
 
 namespace VicoldGis.Algorithms
 {
-    public struct MsqContourLine
-    {
-        public IntPtr px;
-        public IntPtr py;
-        public IntPtr pz;
-        public int nx;
-        public int ny;
-        public int nz;
-        public int pointCount;
-        public float value;
-    };
-
-    public struct MsqTracePramas
-    {
-        public IntPtr beginLineGroupFunc;
-        public IntPtr lineFunc;
-        public IntPtr endLineGroupFunc;
-        public IntPtr userData;
-        public IntPtr analysisValues;
-        public int analysisValueCount;
-        public float invalidValue;
-        public byte isCrossoverAutoOffset;
-        public byte smoothInterpolationCount;
-    };
-
-    public struct MsqGridSlice
-    {
-        public IntPtr pData;
-        public int startX;
-        public int startY;
-        public int width;
-        public int height;
-        public int rowPitch;
-        public float longitude;
-        public float latitude;
-        public float lonInterval;
-        public float latInterval;
-    }
-
     public static class ContourVicoldAlgo
     {
+        private struct MsqContourLine
+        {
+            public IntPtr px;
+            public IntPtr py;
+            public IntPtr pz;
+            public int nx;
+            public int ny;
+            public int nz;
+            public int pointCount;
+            public float value;
+        };
+
+        private struct MsqTracePramas
+        {
+            public IntPtr beginLineGroupFunc;
+            public IntPtr lineFunc;
+            public IntPtr endLineGroupFunc;
+            public IntPtr userData;
+            public IntPtr analysisValues;
+            public int analysisValueCount;
+            public float invalidValue;
+            public byte isCrossoverAutoOffset;
+            public byte smoothInterpolationCount;
+        };
+
+        private struct MsqGridSlice
+        {
+            public IntPtr pData;
+            public int startX;
+            public int startY;
+            public int width;
+            public int height;
+            public int rowPitch;
+            public float longitude;
+            public float latitude;
+            public float lonInterval;
+            public float latInterval;
+        }
+
         [System.Security.SuppressUnmanagedCodeSecurity()]
         [DllImport(@".\Libs\msq.dll", EntryPoint = "msq_trace_contours", CallingConvention = CallingConvention.Cdecl)]
         private unsafe static extern int msq_trace_contours(MsqGridSlice gridSlice, MsqTracePramas tracePramas, IntPtr allocator);
@@ -60,6 +60,7 @@ namespace VicoldGis.Algorithms
                 return CreateContourLinesDo(pp, startX, startY, xSize, ySize, pitch, anaValue, isCrossoverAutoOffset, smoothCount, undefValue, 0, 0, 1f, 1f, isDebug);
             }
         }
+
         public unsafe static ContourLine[] CreateContourLines(float[] data, int startX, int startY, int xSize, int ySize, int pitch,
            float[] anaValue, bool isCrossoverAutoOffset, byte smoothCount, float undefValue = 9999.0F,
            float lon = 0, float lat = 0, float lonIn = 1f, float latIn = 1f, bool isDebug = false)
--- src/VicoldUtility4Net5/VicoldGis/Entities/SpaceData.cs
+++ src/VicoldUtility4Net5/VicoldGis/Entities/SpaceData.cs
@@ -9,5 +9,27 @@ namespace VicoldGis.Entities
         public int Width { get; set; }
         public int Height { get; set; }
         public float[] Data { get; set; }
+
+        public float[] CreateAnaValue(int count = 15)
+        {
+            var min = float.MaxValue;
+            var max = float.MinValue;
+            var len = Data.Length;
+            for (var i = 0; i < len; i++)
+            {
+                var v = Data[i];
+                if (v < min) min = v;
+                if (v > max) max = v;
+            }
+
+            var result = new float[count];
+            int step = (int)(max-min)/ count;
+            for(var i = 0; i < count; i++)
+            {
+                result[i] = min + step * i;
+            }
+
+            return result;
+        }
     }
 }
