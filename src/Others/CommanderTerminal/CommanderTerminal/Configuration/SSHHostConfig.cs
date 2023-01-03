3b7fed4537d4ba9039b5f1c1573df5702782ac97
yiny <yiny@yusur.com>
2023-01-03 18:14:14 +0800

    feat: CommanderTerminal

---
+++ src/Others/CommanderTerminal/CommanderTerminal/Configuration/SSHHostConfig.cs
@@ -0,0 +1,17 @@
+﻿using CommanderTerminal.Configuration.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommanderTerminal.Configuration
+{
+    internal class SSHHostConfig
+    {
+        public SSHHostConfigEtt GetConfigEtt()
+        {
+
+        }
+    }
+}
