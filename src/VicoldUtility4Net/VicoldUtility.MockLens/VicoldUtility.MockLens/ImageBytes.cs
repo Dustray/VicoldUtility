7c4e41e65e19455cdb30975d80abecfd18b06390
Dustray <yinxiyue1995@outlook.com>
2023-03-01 00:15:33 +0800

    feat: lens

---
+++ src/VicoldUtility4Net/VicoldUtility.MockLens/VicoldUtility.MockLens/ImageBytes.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VicoldUtility.MockLens.Algorithms;
+
+namespace VicoldUtility.MockLens
+{
+    internal class ImageByteChannel
+    {
+        public ImageByteChannel(int length)
+        {
+            Data = new byte[length];
+        }
+
+        public byte[] Data { get; set; }
+
+        public byte this[int index]
+        {
+            get => Data[index];
+            set => Data[index] = value;
+        }
+    }
+
+    internal class ImageByteBuffer
+    {
+        public ImageByteBuffer(byte[] data, int width, int height)
+        {
+            SourceData = data;
+            Data = new byte[SourceData.Length];
+            SourceData.CopyTo(Data, 0);
+
+            Width = width;
+            Height = height;
+        }
+
+        public byte[] SourceData { get; }
+
+        public byte[] Data { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public void Add(short value)
+        {
+            for (var i = 0; i < SourceData.Length; i++)
+            {
+                Data[i] = (byte)Math.Max(0, Math.Min(255, SourceData[i] + value));
+            }
+        }
+    }
+
+    internal class ImageBytes
+    {
+        private ImageByteChannel _r { get; }
+        private ImageByteChannel _g { get; }
+        private ImageByteChannel _b { get; }
+        private ImageByteChannel _a { get; }
+
+        public ImageBytes(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            var len = Width * Height;
+            _r = new ImageByteChannel(len);
+            _g = new ImageByteChannel(len);
+            _b = new ImageByteChannel(len);
+            _a = new ImageByteChannel(len);
+        }
+
+        public ImageBytes(Bitmap bitmap)
+        {
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+            var len = Width * Height;
+            _r = new ImageByteChannel(len);
+            _g = new ImageByteChannel(len);
+            _b = new ImageByteChannel(len);
+            _a = new ImageByteChannel(len);
+
+            var data = bitmap.LockBits(new Rectangle(0, 0, Width, Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            unsafe
+            {
+                var ptr = (byte*)data.Scan0;
+                for (int i = 0; i < len; i++)
+                {
+                    _b[i] = ptr[0];
+                    _g[i] = ptr[1];
+                    _r[i] = ptr[2];
+                    _a[i] = ptr[3];
+                    ptr += 4;
+                }
+            }
+            bitmap.UnlockBits(data);
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public int Deep { get; } = 4;
+
+        public byte this[int x, int y, int channel]
+        {
+            get
+            {
+                var index = y * Width + x;
+                switch (channel)
+                {
+                    case 0:
+                        return _r.Data[index];
+                    case 1:
+                        return _g.Data[index];
+                    case 2:
+                        return _b.Data[index];
+                    case 3:
+                        return _a.Data[index];
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(channel));
+                }
+            }
+            set
+            {
+                var index = y * Width + x;
+                switch (channel)
+                {
+                    case 0:
+                        _r.Data[index] = value;
+                        break;
+                    case 1:
+                        _g.Data[index] = value;
+                        break;
+                    case 2:
+                        _b.Data[index] = value;
+                        break;
+                    case 3:
+                        _a.Data[index] = value;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(channel));
+                }
+            }
+        }
+
+        public ImageByteBuffer CreateSourceCopy()
+        {
+            byte[] copies = new byte[Width * Height * Deep];
+            int i = 0;
+            for (int x = 0; x < Width; x += 4)
+            {
+                for (int y = 0; y < Height; y += 4)
+                {
+                    int index = y * Width + x;
+                    copies[i++] = _b.Data[index];//B
+                    copies[i++] = _g.Data[index];//G
+                    copies[i++] = _r.Data[index];//R
+                    copies[i++] = _a.Data[index];//A
+                }
+            }
+
+            return new ImageByteBuffer(copies, Width, Height);
+        }
+
+        public ImageByteBuffer CreateSourceZoomCopy(int width, int height)
+        {
+            byte[] copies = new byte[width * height * Deep];
+            int i = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    float zoomX = (float)Width * ((float)x / width);
+                    float zoomY = (float)Height * ((float)y / height);
+
+                    var b = BilinearInterpolationAlgorithm.Execute(_b.Data, Width, Height, zoomX, zoomY);
+                    var g = BilinearInterpolationAlgorithm.Execute(_g.Data, Width, Height, zoomX, zoomY);
+                    var r = BilinearInterpolationAlgorithm.Execute(_r.Data, Width, Height, zoomX, zoomY);
+                    var a = BilinearInterpolationAlgorithm.Execute(_a.Data, Width, Height, zoomX, zoomY);
+
+                    copies[i++] = b;//B
+                    copies[i++] = g;//G
+                    copies[i++] = r;//R
+                    copies[i++] = a;//A
+                }
+            }
+
+            return new ImageByteBuffer(copies, width, height);
+        }
+    }
+}
---
+++ src/VicoldUtility4Net/VicoldUtility.MockLens/VicoldUtility.MockLens/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VicoldUtility.MockLens
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ImageBytes? imageBytes_ = null;
+        private WriteableBitmap? writeableBitmap_ = null;
+        private ImageByteBuffer? buffer_ = null;
+        private bool updating = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // read file to bitmap
+            using Bitmap bitmap = new Bitmap(@"F:\照片\2023.01.01元旦青岛\DSC01645.JPG");
+            // get orientation from id: 274
+            var orientation = bitmap.PropertyItems.FirstOrDefault(x => x.Id == 274);
+
+            if (orientation is { } && orientation.Value is { })
+            {
+                int value = orientation.Value[0];
+                Rotating(bitmap, value);
+            }
+
+            imageBytes_ = new ImageBytes(bitmap);
+            (int newWidth, int newHeight) = GetImageSizeByMaxSize((int)ImageSourceFrame.ActualWidth, (int)ImageSourceFrame.ActualHeight);
+            LoadBitmap(newWidth, newHeight);
+        }
+        private (int width, int height) GetImageSizeByMaxSize(int maxWidth, int maxHeight)
+        {
+            if (imageBytes_ is not { })
+            {
+                return (0, 0);
+            }
+
+            var width = imageBytes_.Width;
+            var height = imageBytes_.Height;
+            if (width > maxWidth || height > maxHeight)
+            {
+                var ratio = (double)width / height;
+                if (ratio > 1)
+                {
+                    width = maxWidth;
+                    height = (int)(width / ratio);
+                }
+                else
+                {
+                    height = maxHeight;
+                    width = (int)(height * ratio);
+                }
+            }
+
+            return (width, height);
+        }
+
+        /// <summary>
+        /// 加载图片（当尺寸变化）
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        private void LoadBitmap(int width, int height)
+        {
+            if (imageBytes_ is not { })
+            {
+                return;
+            }
+
+            if (buffer_ is not { })
+            {
+                buffer_ = imageBytes_.CreateSourceZoomCopy(width, height);
+            }
+            else if (width - buffer_.Width > 10 || height - buffer_.Height > 10)
+            {
+                buffer_ = imageBytes_.CreateSourceZoomCopy(width, height);
+            }
+            else
+            {
+                return;
+            }
+            writeableBitmap_ = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+            ImageSource.Source = writeableBitmap_;
+
+            Update();
+        }
+
+        public void Update()
+        {
+            if (updating)
+            {
+                return;
+            }
+
+            if (buffer_ is not { })
+            {
+                return;
+            }
+
+            byte[]? buffer = buffer_.Data;
+            if (buffer is not { })
+            {
+                return;
+            }
+
+            //this.Dispatcher.Invoke(() =>
+            //{
+            updating = true;
+            writeableBitmap_?.WritePixels(new Int32Rect(0, 0, buffer_.Width, buffer_.Height), buffer, writeableBitmap_.BackBufferStride, 0);
+            updating = false;
+            //});
+        }
+        public static void Rotating(Bitmap img, int orien)
+        {
+            switch (orien)
+            {
+                case 2:
+                    img.RotateFlip(RotateFlipType.RotateNoneFlipX); //horizontal flip
+                    break;
+                case 3:
+                    img.RotateFlip(RotateFlipType.Rotate180FlipNone); //right-top
+                    break;
+                case 4:
+                    img.RotateFlip(RotateFlipType.RotateNoneFlipY); //vertical flip
+                    break;
+                case 5:
+                    img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    break;
+                case 6:
+                    img.RotateFlip(RotateFlipType.Rotate90FlipNone); //right-top
+                    break;
+                case 7:
+                    img.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    break;
+                case 8:
+                    img.RotateFlip(RotateFlipType.Rotate270FlipNone); //left-bottom
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            (int newWidth, int newHeight) = GetImageSizeByMaxSize((int)ImageSourceFrame.ActualWidth, (int)ImageSourceFrame.ActualHeight);
+            LoadBitmap(newWidth, newHeight);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (buffer_ is not { })
+            {
+                return;
+
+            }
+            buffer_.Add((short)AddSlider.Value);
+            Update();
+        }
+    }
+
+}
